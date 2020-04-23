824dd66dffcc396eb68b531dc6541a775f2b2737
Bruno Ribeiro <brunomestres@gmail.com>
2020-04-23 00:34:56 -0300

    .

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost/subscribers',{
+const subscribersRouter = require('./routes/Subscribers');
+
+
+mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
@@ -12,8 +16,12 @@ const db = mongoose.connection;
 db.on('error', (err)=>{
     console.log(err);
 });
-
 db.once('open',()=> console.log('Database Connected'))
+
+app.use(express.json());
+
+app.use('/subscribers', subscribersRouter);
+
 
 app.listen(3000,()=>{
     console.log('Servidor na porta 3000');
